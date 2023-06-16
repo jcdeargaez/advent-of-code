ecd35cefe9b5a5b0431f5417d374e3e40d871649
Juan Argaez <jc.argaez@gmail.com>
2023-06-16 10:30:35 -0500

    add modules and range ctor

--- 2022/04/solution.fsx
+++ 2022/04/solution.fsx
@@ -1,45 +1,63 @@
 open System.IO
 
-type Range = {
-    Begin : int
-    End : int
-}
+module Domain =
+    type Range = {
+        Begin : int
+        End : int
+    }
 
-type Pair = {
-    Left : Range
-    Right : Range
-}
+    type Pair = {
+        Left : Range
+        Right : Range
+    }
 
-let fullyContained {Right = right; Left = left} =
-    (left.Begin <= right.Begin && right.End <= left.End) || (right.Begin <= left.Begin && left.End <= right.End)
+module Range =
+    open Domain
 
-let overlapAtAll {Right = right; Left = left} =
-    (left.Begin <= right.Begin && right.Begin <= left.End) || (right.Begin <= left.Begin && left.Begin <= right.End)
+    let create b e =
+        if e < b then invalidArg (nameof e) "e is lower than b"
+        {Begin = b; End = e}
 
-let count overlap pairs =
-    pairs
-    |> Seq.filter overlap
-    |> Seq.length
+module Operations =
+    open Domain
 
-let parseLine (line : string) =
-    let pairs =
-        line.Split ','
-        |> Array.map (fun range ->
-            match range.Split '-' with
-            | [| b; e |] -> {Begin = int b; End = int e}
-            | _ -> failwith "Invalid format")
-    {Left = pairs.[0]; Right = pairs.[1]}
+    let fullyContained {Right = right; Left = left} =
+        (left.Begin <= right.Begin && right.End <= left.End) || (right.Begin <= left.Begin && left.End <= right.End)
 
-let pairs =
-    "input.txt"
-    |> File.ReadLines
-    |> Seq.map parseLine
-    |> Seq.toList
+    let overlapAtAll {Right = right; Left = left} =
+        (left.Begin <= right.Begin && right.Begin <= left.End) || (right.Begin <= left.Begin && left.Begin <= right.End)
 
-pairs
-|> count fullyContained
+    let count overlap pairs =
+        pairs
+        |> Seq.filter overlap
+        |> Seq.length
+
+module Api =
+    open Domain
+    open Operations
+
+    let parseLine (line : string) =
+        let pairs =
+            line.Split ','
+            |> Array.map (fun range ->
+                match range.Split '-' with
+                | [| b; e |] -> Range.create (int b) (int e)
+                | _ -> failwith "Invalid format")
+        {Left = pairs.[0]; Right = pairs.[1]}
+
+    let readInput path =
+        path
+        |> File.ReadLines
+        |> Seq.map parseLine
+        |> Seq.toList
+
+    let part1 = count fullyContained
+    let part2 = count overlapAtAll
+
+let pairs = Api.readInput "input.txt"
+
+Api.part1 pairs
 |> printfn "Part 1: %i"
 
-pairs
-|> count overlapAtAll
+Api.part2 pairs
 |> printfn "Part 2: %i"
