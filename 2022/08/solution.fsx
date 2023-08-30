5ff1a2c93651846daa908ac73fc51984d96a472d
Juan Argaez <jc.argaez@gmail.com>
2023-08-30 10:39:44 -0500

    refactor

--- 2022/08/solution.fsx
+++ 2022/08/solution.fsx
@@ -6,101 +6,65 @@ let input =
         line.ToCharArray ()
         |> Array.map (string >> int))
 
-let part1 =
-    let up =
-        let m = Array.init input.Length (fun i -> Array.replicate input.[i].Length 0)
-        input.[0]
-        |> Seq.iteri (fun j _ ->
+let vertically f =
+    let m = Array.init input.Length (fun _ -> Array.replicate input.[0].Length 0)
+    input.[0]
+    |> Array.iteri (fun j _ ->
+        let column = 
             input
-            |> Seq.map (Array.item j)
-            |> Seq.scan max 0
-            |> Seq.tail
-            |> Seq.iteri (fun i v -> m.[i].[j] <- v))
-        m
+            |> Array.map (Array.item j)
+        f column
+        |> Array.iteri (fun i v -> m.[i].[j] <- v))
+    m
 
-    let left =
-        Array.init input.Length (fun i ->
-            input.[i]
-            |> Array.scan max 0
-            |> Array.tail)
+let horizontally f = Array.init input.Length (fun i -> f input.[i])
 
-    let down =
-        let m = Array.init input.Length (fun i -> Array.replicate input.[i].Length 0)
-        input.[0]
-        |> Seq.iteri (fun j _ ->
-            let col =
-                input
-                |> Seq.map (Array.item j)
-            Seq.scanBack max col 0
-            |> Seq.take input.Length
-            |> Seq.iteri (fun i v -> m.[i].[j] <- v))
-        m
+let part1 =
+    let scanMax = Array.scan max 0 >> Array.tail
+    let scanBackMax items =
+        Array.scanBack max items 0
+        |> Array.take items.Length
 
-    let right =
-        Array.init input.Length (fun i ->
-            Array.scanBack max input.[i] 0
-            |> Array.take input.[i].Length)
+    let up = vertically scanMax
+    let left = horizontally scanMax
+    let down = vertically scanBackMax
+    let right = horizontally scanBackMax
+
+    let visible i j =
+        let h = input.[i].[j]
+        h > up.[i - 1].[j] || h > left.[i].[j - 1] || h > down.[i + 1].[j] || h > right.[i].[j + 1]
 
     let edge = input.Length * 2 + (input.[0].Length - 2) * 2
     let interior =
         {1..input.Length - 2}
         |> Seq.sumBy (fun i ->
             {1..input.[i].Length - 2}
-            |> Seq.filter (fun j ->
-                let h = input.[i].[j]
-                h > up.[i - 1].[j] || h > left.[i].[j - 1] || h > down.[i + 1].[j] || h > right.[i].[j + 1])
+            |> Seq.filter (fun j -> visible i j)
             |> Seq.length)
     
     edge + interior
 
 let part2 =
-    let scenicScores heights =
-        let indices = Array.replicate 10 0
+    let visibility heights =
+        let idxByHeight = Array.replicate 10 0
         heights
-        |> Seq.mapi (fun i h ->
-            let score = i - (Array.max indices.[h..])
-            indices.[h] <- i
-            score)
-        |> Seq.toArray
+        |> Array.mapi (fun i h ->
+            let distance = i - (Array.max idxByHeight.[h..])
+            idxByHeight.[h] <- i
+            distance)
+    let visibilityBack = Array.rev >> visibility >> Array.rev
 
-    let up =
-        let m = Array.init input.Length (fun i -> Array.replicate input.[i].Length 0)
-        input.[0]
-        |> Seq.iteri (fun j _ ->
-            input
-            |> Seq.map (Array.item j)
-            |> scenicScores
-            |> Seq.iteri (fun i v -> m.[i].[j] <- v))
-        m
+    let up = vertically visibility
+    let left = horizontally visibility
+    let down = vertically visibilityBack
+    let right = horizontally visibilityBack
 
-    let left =
-        Array.init input.Length (fun i ->
-            input.[i]
-            |> scenicScores)
-
-    let down =
-        let m = Array.init input.Length (fun i -> Array.replicate input.[i].Length 0)
-        input.[0]
-        |> Seq.iteri (fun j _ ->
-            input
-            |> Array.map (Array.item j)
-            |> Array.rev
-            |> scenicScores
-            |> Array.rev
-            |> Array.iteri (fun i v -> m.[i].[j] <- v))
-        m
-
-    let right =
-        Array.init input.Length (fun i ->
-            input.[i]
-            |> Array.rev
-            |> scenicScores
-            |> Array.rev)
+    let scenicScore i j = up.[i].[j] * left.[i].[j] * down.[i].[j] * right.[i].[j]
 
     {1..input.Length - 2}
     |> Seq.map (fun i ->
         {1..input.[i].Length - 2}
-        |> Seq.map (fun j -> up.[i].[j] * left.[i].[j] * down.[i].[j] * right.[i].[j])
+        |> Seq.map (fun j -> scenicScore i j)
         |> Seq.max)
     |> Seq.max
 
