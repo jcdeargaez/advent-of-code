369186207d350a91e33c4566962f3df331aef32a
Juan Argaez <jc.argaez@gmail.com>
2023-12-26 18:58:55 -0500

    improvement

--- 2022/20/solution.fsx
+++ 2022/20/solution.fsx
@@ -1,61 +1,31 @@
 open System.IO
 
-let mix (input : ResizeArray<int * int64>) =
-    let move i =
-        while fst input.[0] <> i do
-            let n = input.[0]
-            input.RemoveAt 0
-            input.Add n
+let mix (items : int64 array) (indices : int ResizeArray) =
+    let count = indices.Count - 1 |> int64
+    for i in 0..indices.Count - 1 do
+        let j = indices.IndexOf i
+        indices.RemoveAt j
+        let k = ((int64 j + items.[i]) % count + count) % count
+        indices.Insert (int k, i)
 
-        let item = input.[0]
-        input.RemoveAt 0
+let sumIndices (items : int64 array) (indices : int ResizeArray) =
+    let i0 = items |> Array.findIndex ((=) 0) |> indices.IndexOf
+    {1..3} |> Seq.sumBy (fun i -> items.[indices.[(i0+i*1000) % indices.Count]])
 
-        {0L .. (snd item |> abs) % int64 input.Count - 1L}
-        |> Seq.iter (fun _ ->
-            if snd item > 0 then
-                let n = input.[0]
-                input.RemoveAt 0
-                input.Add n
-            else
-                let last = input.Count - 1
-                let n = input.[last]
-                input.RemoveAt last
-                input.Insert (0, n))
-        
-        if snd item >= 0 then input.Add item
-        else input.Insert (0, item)
-
-    {0..input.Count - 1}
-    |> Seq.iter move
-
-let sumIndices (items : ResizeArray<int * int64>) =
-    let i0 =
-        items
-        |> Seq.findIndex (snd >> (=) 0)
-    
-    {1..3}
-    |> Seq.sumBy (fun i -> snd items.[(i * 1000 + i0) % items.Count])
-
-let input =
-    File.ReadAllLines "input.txt"
-    |> Array.map int64
-    |> Array.indexed
+let input = File.ReadLines "input.txt" |> Seq.map int64 |> Seq.toArray
 
 let part1 =
-    let items = ResizeArray input
-    mix items
-    sumIndices items
+    let indices = ResizeArray {0..input.Length - 1}
+    mix input indices
+    sumIndices input indices
 
 printfn "Part 1: %i" part1
 
 let part2 =
-    let items =
-        input
-        |> Seq.map (fun (i, v) -> i, v * 811589153L)
-        |> ResizeArray
-
-    {1..10}
-    |> Seq.iter (fun _ -> mix items)
-    sumIndices items
+    let keyInput = input |> Array.map (( * ) 811589153L)
+    let indices = ResizeArray {0..input.Length - 1}
+    for _ in 1..10 do
+        mix keyInput indices
+    sumIndices keyInput indices
 
 printfn "Part 2: %i" part2
