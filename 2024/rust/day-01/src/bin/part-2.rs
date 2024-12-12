6b25dd0a01e2b64a0e72bbce0fdfb4ac05438aef
Juan Argaez <jc.argaez@gmail.com>
2024-12-11 22:16:06 -0500

    fix gitignore

---
+++ 2024/rust/day-01/src/bin/part-2.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+use day_01::part2;
+
+fn main() {
+    let content = fs::read_to_string("./input.txt").unwrap();
+    println!("Part 2: {}", part2(&content));
+}
---
+++ 2024/rust/day-03/src/bin/part-1.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+use day_03::part1;
+
+fn main() {
+    let content = fs::read_to_string("./input.txt").unwrap();
+    println!("Part 1: {}", part1(&content));
+}
